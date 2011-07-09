e88a8bdb1fb66bf1604fa3bfe557522e684daa63
Bill DeRusha <Bill.DeRusha@gmail.com>
2011-07-09 17:08:07 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Billderusha::Application.config.secret_token = '0e9bdc9b5ff4c6eeb17b2c063cc4c559f53b241c5e0f8267b6aab86d8cbcaee3bd1fe04a19af014c4d674c8bc6526b18dcb9d0f89e28ab7b806b035642624c53'
