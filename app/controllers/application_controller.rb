ca7ce634dccfd6c75757b8f64dc154cad1e27ced
Bill DeRusha <Bill.DeRusha@gmail.com>
2011-07-09 20:45:27 -0400

    host with port substring fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_filter :check_uri
 
   def check_uri
-    redirect_to request.protocol + request.host_with_port[4...-1] + request.request_uri if /^www/.match(request.host)
+    redirect_to request.protocol + request.host_with_port[4..-1] + request.request_uri if /^www/.match(request.host)
   end
 
 end
