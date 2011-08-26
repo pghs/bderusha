aea2b5dd63908cbfd70f7f5712d5617f13f0f5b0
Bill DeRusha <Bill.DeRusha@gmail.com>
2011-08-26 11:38:43 -0400

    google verifcation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,6 @@ class ApplicationController < ActionController::Base
   before_filter :check_uri
 
   def check_uri
-    puts request.protocol
-    puts request.host_with_port
-    puts request.host
-    puts request.request_uri
     redirect_to request.protocol + request.host_with_port[4..-1] + request.request_uri if /^www/.match(request.host)
   end
 
